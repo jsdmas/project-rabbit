0430934595a8b78e95e86e9bdca7dc90032995df
jsdmas <sdmas5730@gmail.com>
2023-05-14 13:06:21 +0900

    test : reactRouter notLazy

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,32 +1,28 @@
-import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Spinner from "./components/Spinner";
-const EditThread = lazy(() => import('./routes/EditThread'));
-const Home = lazy(() => import('./routes/Home'));
-const Join = lazy(() => import('./routes/Join'));
-const Login = lazy(() => import('./routes/Login'));
-const NotFound = lazy(() => import('./routes/NotFound'));
-const Thread = lazy(() => import('./routes/Thread'));
-const Write = lazy(() => import('./routes/Write'));
-const UserProfile = lazy(() => import('./routes/UserProfile'));
-const ChangePassword = lazy(() => import('./routes/ChangePassword'));
+import EditThread from './routes/EditThread'
+import Home from './routes/Home'
+import Join from './routes/Join'
+import Login from './routes/Login'
+import NotFound from './routes/NotFound'
+import Thread from './routes/Thread'
+import Write from './routes/Write'
+import UserProfile from './routes/UserProfile'
+import ChangePassword from './routes/ChangePassword'
 
 const Router = () => {
     return (
         <BrowserRouter>
-            <Suspense fallback={<Spinner />}>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/join" element={<Join />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/write" element={<Write />} />
-                    <Route path="/user/:userid" element={<UserProfile />} />
-                    <Route path="/user/change-password" element={<ChangePassword />} />
-                    <Route path="/thread/:threadid" element={<Thread />} />
-                    <Route path="/thread/:threadid/edit" element={<EditThread />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </Suspense>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/join" element={<Join />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/write" element={<Write />} />
+                <Route path="/user/:userid" element={<UserProfile />} />
+                <Route path="/user/change-password" element={<ChangePassword />} />
+                <Route path="/thread/:threadid" element={<Thread />} />
+                <Route path="/thread/:threadid/edit" element={<EditThread />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
         </BrowserRouter>
     );
 };
