986e34af00a0f9a63e79f630c49f87079efa1819
jsdmas <sdmas5730@gmail.com>
2023-05-15 14:19:52 +0900

    test : add Throttle

--- src/helper/throttle.ts
+++ src/helper/throttle.ts
@@ -2,6 +2,7 @@
 export const throttle = (func: Function, limit: number | undefined) => {
     let inThrottle: boolean;
     return function (this: any, ...args: any[]) {
+        // 사이드 이펙트 방지
         const context = this;
         if (!inThrottle) {
             func.apply(context, args);
