e0aa78dadfe8a7098c6db7b4d74cbe30bf4c4d68
jsdmas <sdmas5730@gmail.com>
2023-07-10 17:50:27 +0900

    :recycle: Refactor : router path

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Spinner from './components/Spinner';
+import { ROUTER_PATH } from './constants/path';
 
 const EditThread = lazy(() => import('./routes/EditThread'));
 const Home = lazy(() => import('./routes/Home'));
@@ -18,19 +19,19 @@ const Router = () => {
     <BrowserRouter>
       <Suspense fallback={<Spinner />}>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/join" element={<Join />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/write" element={<Write />} />
-          <Route path="/user">
-            <Route path=":userid" element={<UserProfile />} />
-            <Route path="change-password" element={<ChangePassword />} />
+          <Route path={ROUTER_PATH.HOME} element={<Home />} />
+          <Route path={ROUTER_PATH.JOIN} element={<Join />} />
+          <Route path={ROUTER_PATH.LOGIN} element={<Login />} />
+          <Route path={ROUTER_PATH.WRITE} element={<Write />} />
+          <Route path={ROUTER_PATH.USER}>
+            <Route path={ROUTER_PATH.USER_ID} element={<UserProfile />} />
+            <Route path={ROUTER_PATH.USER_PASSWORD} element={<ChangePassword />} />
           </Route>
-          <Route path="/thread">
-            <Route path=":threadid" element={<Thread />} />
-            <Route path=":threadid/edit" element={<EditThread />} />
+          <Route path={ROUTER_PATH.THREAD}>
+            <Route path={ROUTER_PATH.THREAD_ID} element={<Thread />} />
+            <Route path={ROUTER_PATH.THREAD_EDIT} element={<EditThread />} />
           </Route>
-          <Route path="*" element={<NotFound />} />
+          <Route path={ROUTER_PATH.ALL} element={<NotFound />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
